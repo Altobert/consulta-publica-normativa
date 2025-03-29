b8d709b9b48ccfa8c0e6443009905876d1adf109
Altobert <alberto.sanmartin@usach.cl>
2025-03-29 16:55:52 -0300

    se agregan componentes

--- src/components/CabeceraSII.vue
+++ src/components/CabeceraSII.vue
@@ -1,5 +1,9 @@
 <template>
-  <h1>Cabecera</h1>
+  <div class="cabecera">
+    <img src="../assets/logo.png" alt="Logo SII" class="logo-sii"/>
+    <h1>Consulta Publica</h1>
+    <p>Consulta Publica de la aplicacion SII</p>
+  </div>
 </template>
 
 <script setup>
