71cef6ede07c40d341474c14b03c750b8cc10361
Altobert <alberto.sanmartin@usach.cl>
2025-03-29 17:12:58 -0300

    se agrega lo necesario

--- src/components/CabeceraSII.vue
+++ src/components/CabeceraSII.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cabecera">
-    <img src="../assets/logo.png" alt="Logo SII" class="logo-sii"/>
+    <img src="../assets/logoSII250x108.jpg" alt="Logo SII" class="logo-sii"/>
     <h1>Consulta Publica</h1>
     <p>Consulta Publica de la aplicacion SII</p>
   </div>
